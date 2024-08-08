298bcf8fd6a56fb6ddfb126b255f4cda68d87376
Maksymilian <max.szewczyk@interia.pl>
2024-08-08 23:55:27 +0200

    implemented the basic form structure for the AskQuestion page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+"use client";
+import Question from "@/components/forms/Question";
+import React from "react";
 const AskQuestion = () => {
-  return (
-    <div>AskQuestion</div>
-  )
-}
+    return (
+        <div>
+            <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+            <div className="mt-9">
+                <Question />
+            </div>
+        </div>
+    );
+};
 
-export default AskQuestion
+export default AskQuestion;

01d67e4b7d8b169a41ccee1508e973e86568a9e2
Maksymilian <max.szewczyk@interia.pl>
2024-08-14 00:59:31 +0200

    added functionality for adding tags

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -8,7 +8,8 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
-
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 const Question = () => {
     const editorRef = useRef(null);
 
@@ -27,6 +28,36 @@ const Question = () => {
         // ✅ This will be type-safe and validated.
         console.log(values);
     }
+
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+        if (e.key === "Enter" && field.name === "tags") {
+            e.preventDefault();
+
+            const tagInput = e.target as HTMLInputElement;
+            const tagValue = tagInput.value.trim();
+
+            if (tagValue !== "") {
+                if (tagValue.length > 15) {
+                    return form.setError("tags", {
+                        type: "required",
+                        message: "Tag must be less than 15 characters",
+                    });
+                }
+
+                if (!field.value.includes(tagValue as never)) {
+                    form.setValue("tags", [...field.value, tagValue]);
+                    tagInput.value = "";
+                    form.clearErrors("tags");
+                } else {
+                    form.trigger();
+                }
+            }
+        }
+    };
+    const handleTagRemove = (tag: string, field: any) => {
+        const newTags = field.value.filter((t: string) => t !== tag);
+        form.setValue("tags", newTags);
+    };
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
@@ -112,11 +143,32 @@ const Question = () => {
                                 Tags <span className="text-primary-500">*</span>
                             </FormLabel>
                             <FormControl className="mt-3.5">
-                                <Input
-                                    className="no-focus paragraph-regular background-light800_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
-                                    placeholder="Add tags..."
-                                    {...field}
-                                />
+                                <>
+                                    <Input
+                                        className="no-focus paragraph-regular background-light800_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
+                                        placeholder="Add tags..."
+                                        onKeyDown={(e) => handleInputKeyDown(e, field)}
+                                    />
+                                    {field.value.length > 0 && (
+                                        <div className="flex-start mt-2.5 gap-2.5">
+                                            {field.value.map((tag: any) => (
+                                                <Badge
+                                                    key={tag}
+                                                    className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md px-4 py-2 capitalize">
+                                                    <p>{tag}</p>
+                                                    <Image
+                                                        src="/assets/icons/close.svg"
+                                                        width={12}
+                                                        height={12}
+                                                        alt=""
+                                                        className="cursor-pointer object-contain invert-0 dark:invert"
+                                                        onClick={() => handleTagRemove(tag, field)}
+                                                    />
+                                                </Badge>
+                                            ))}
+                                        </div>
+                                    )}
+                                </>
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Add up to 5 tags to describe what your question is about. Start typing to see
